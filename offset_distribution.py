0eebb32b4398419bf0b3b1320e92ab292ac7d4b2
MyNameIsTrez <sbos@student.codam.nl>
2023-08-11 15:53:35 +0200

    Explain offset_distribution.py

--- offset_distribution.py
+++ offset_distribution.py
@@ -1,7 +1,12 @@
+# Shows what the random mob offset distribution looks like in performSpawningNearChunk()
+
 import random
 
 seen = {}
+
 for _ in range(100000):
     i = random.randrange(6) - random.randrange(6)
+
     seen[i] = seen.get(i, 0) + 1
+
 print(seen)
